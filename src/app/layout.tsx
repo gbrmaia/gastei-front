21d8aee20b19a8e013e21b1d0df3113f92b94794
Gabriel Maia <gabrielmaia43@hotmail.com>
2024-09-17 12:01:41 -0300

    :wrench: configuração metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,31 +6,24 @@ import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 
 export const metadata: Metadata = {
-  metadataBase: new URL(
-    process.env.APP_URL
-      ? `${process.env.APP_URL}`
-      : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : `http://localhost:${process.env.PORT || 3000}`
-  ),
-  title: "shadcn/ui sidebar",
+  title: "Gastei",
   description:
-    "A stunning and functional retractable sidebar for Next.js built on top of shadcn/ui complete with desktop and mobile responsiveness.",
+    "Controle suas finanças. Gastei é um aplicativo de controle financeiro pessoal que ajuda você a organizar suas finanças.",
   alternates: {
     canonical: "/"
   },
   openGraph: {
     url: "/",
-    title: "shadcn/ui sidebar",
+    title: "Gastei",
     description:
-      "A stunning and functional retractable sidebar for Next.js built on top of shadcn/ui complete with desktop and mobile responsiveness.",
+      "Controle suas finanças. Gastei é um aplicativo de controle financeiro pessoal que ajuda você a organizar suas finanças.",
     type: "website"
   },
   twitter: {
     card: "summary_large_image",
-    title: "shadcn/ui sidebar",
+    title: "Gastei",
     description:
-      "A stunning and functional retractable sidebar for Next.js built on top of shadcn/ui complete with desktop and mobile responsiveness."
+      "Controle suas finanças. Gastei é um aplicativo de controle financeiro pessoal que ajuda você a organizar suas finanças.",
   }
 };
 
